d12f835a444092db622918ba41c55313c9bb2d3c
Vaclavrut <vaclav@smartemailing.cz>
2020-02-26 20:23:59 +0100

    fix for items which doesn't have any asin

--- src/parseItemUrls.js
+++ src/parseItemUrls.js
@@ -4,26 +4,28 @@
 const { getOriginUrl } = require('./utils');
 
 async function extractSellers($, request) {
-    const originUrl = await getOriginUrl(request)
+    const originUrl = await getOriginUrl(request);
     const itemUrls = [];
     const items = $('.s-result-list [data-asin]');
     if (items.length !== 0) {
         items.each(function () {
             const asin = $(this).attr('data-asin');
             const sellerUrl = `${originUrl}/gp/offer-listing/${asin}`;
-            itemUrls.push({
-                url: sellerUrl,
-                asin,
-                detailUrl: `${originUrl}/dp/${asin}`,
-                sellerUrl,
-            });
+            if (asin) {
+                itemUrls.push({
+                    url: sellerUrl,
+                    asin,
+                    detailUrl: `${originUrl}/dp/${asin}`,
+                    sellerUrl,
+                });
+            }
         });
     }
     return itemUrls;
 }
 
-async function parseItemUrls($,request) {
-    const urls = await extractSellers($,request);
+async function parseItemUrls($, request) {
+    const urls = await extractSellers($, request);
     console.log(`Found ${urls.length} on a site, going to crawl them.`);
     return urls;
 }
